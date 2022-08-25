d5242ffa8e0f5480ee96166d543f78d5fb6b1275
Ian <contatoiancunha@gmail.com>
2022-08-25 18:04:07 -0300

    indice

--- src/pages/Indices.jsx
+++ src/pages/Indices.jsx
@@ -16,8 +16,8 @@ function Indices() {
               <div className="card">
                 <div className="card-body">
                   <h5 className="card-title">Índice Marta/Ademi</h5>
-                  <p className="card-text fs-2 fw-semibold">JUN/22</p>
-                  <a target="_blank" href="https://deixacommarta.com.br/download/Indice_Marta_Ademi_JUN2022_v1_202207.pdf" className="btn download">Visualizar / Baixar</a>
+                  <p className="card-text fs-2 fw-semibold">JUL/22</p>
+                  <a target="_blank" href="https://deixacommarta.com.br/download/Indice_Marta_Ademi_JUL2022_202208.pdf" className="btn download">Visualizar / Baixar</a>
                 </div>
               </div>
             </div>
@@ -26,8 +26,8 @@ function Indices() {
               <div className="card">
                 <div className="card-body">
                   <h5 className="card-title">Índice Marta/Ademi</h5>
-                  <p className="card-text fs-2 fw-semibold">JUL/22</p>
-                  <a target="_blank" href="https://deixacommarta.com.br/download/Indice_Marta_Ademi_JUL2022_202208.pdf" className="btn download">Visualizar / Baixar</a>
+                  <p className="card-text fs-2 fw-semibold">JUN/22</p>
+                  <a target="_blank" href="https://deixacommarta.com.br/download/Indice_Marta_Ademi_JUN2022_v1_202207.pdf" className="btn download">Visualizar / Baixar</a>
                 </div>
               </div>
             </div>
