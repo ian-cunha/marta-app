feb7f50a9c08ba498f858e2827143b081f94890e
Ian <contatoiancunha@gmail.com>
2022-10-25 14:36:26 -0300

    indice update 25/10

--- src/pages/Indices.jsx
+++ src/pages/Indices.jsx
@@ -18,6 +18,16 @@ function Indices() {
               <div className="card">
                 <div className="card-body">
                   <h5 className="card-title">Índice Marta/Ademi</h5>
+                  <p className="card-text fs-2 fw-semibold">SET/22</p>
+                  <a target="_blank" href="http://deixacommarta.com.br/download/Indice_Marta_Ademi_SET2022_202209_v1.pdf" className="btn download">Visualizar / Baixar</a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-sm-6 m-2">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Índice Marta/Ademi</h5>
                   <p className="card-text fs-2 fw-semibold">JUL/22</p>
                   <a target="_blank" href="https://deixacommarta.com.br/download/Indice_Marta_Ademi_JUL2022_202208.pdf" className="btn download">Visualizar / Baixar</a>
                 </div>
