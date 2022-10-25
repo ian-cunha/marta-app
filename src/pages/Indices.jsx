f3c15244a73cc7866dfdd33b9bb014097f00c694
Ian <contatoiancunha@gmail.com>
2022-10-25 14:47:20 -0300

    update index 25/10

--- src/pages/Indices.jsx
+++ src/pages/Indices.jsx
@@ -28,6 +28,16 @@ function Indices() {
               <div className="card">
                 <div className="card-body">
                   <h5 className="card-title">Índice Marta/Ademi</h5>
+                  <p className="card-text fs-2 fw-semibold">AGO/22</p>
+                  <a target="_blank" href="https://deixacommarta.com.br/download/Indice_Marta_Ademi_AGO2022.pdf" className="btn download">Visualizar / Baixar</a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-sm-6 m-2">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Índice Marta/Ademi</h5>
                   <p className="card-text fs-2 fw-semibold">JUL/22</p>
                   <a target="_blank" href="https://deixacommarta.com.br/download/Indice_Marta_Ademi_JUL2022_202208.pdf" className="btn download">Visualizar / Baixar</a>
                 </div>
@@ -40,16 +50,6 @@ function Indices() {
                   <h5 className="card-title">Índice Marta/Ademi</h5>
                   <p className="card-text fs-2 fw-semibold">JUN/22</p>
                   <a target="_blank" href="https://deixacommarta.com.br/download/Indice_Marta_Ademi_JUN2022_v1_202207.pdf" className="btn download">Visualizar / Baixar</a>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-sm-6 m-2">
-              <div className="card">
-                <div className="card-body">
-                  <h5 className="card-title">Índice Marta/Ademi</h5>
-                  <p className="card-text fs-2 fw-semibold">AGO/22</p>
-                  <a target="_blank" href="https://deixacommarta.com.br/download/Indice_Marta_Ademi_AGO2022.pdf" className="btn download">Visualizar / Baixar</a>
                 </div>
               </div>
             </div>
