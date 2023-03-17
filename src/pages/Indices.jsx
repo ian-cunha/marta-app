c0326539ef4f9b63dc3893bbeeb1981528d7fb7e
Ian <69704125+ian-cunha@users.noreply.github.com>
2023-03-17 04:03:15 -0300

    update

--- src/pages/Indices.jsx
+++ src/pages/Indices.jsx
@@ -3,7 +3,6 @@ import Footer from '../components/Footer'
 import Whatsapp from '../components/Whatsapp'
 import indiceJSON from "/public/indice.json"
 
-
 function Indices() {
 
   $(document).ready(function () {
@@ -23,15 +22,15 @@ function Indices() {
       <div className='indices'>
         <div className='container-fluid'>
           <div className="d-flex flex-column justify-content-center">
-            <h2 className='text-light m-2'>Índices Marta</h2>
-
-            <input className="form-control pt-3 pb-3 mb-2" id="filterIndice" type="text" placeholder="Pesquise" />
 
             <div className='container' id='indices'>
               <div className='row'>
 
+              <h2 className='text-light m-2'>Índices Marta</h2>
+              <input className="form-control pt-3 pb-3 mb-2 mt-2" id="filterIndice" type="text" placeholder="Pesquise" />
+
                 {indiceJSON && indiceJSON.map(({ nome, estado, data, url }) => (
-                  <div className="col-6 mt-2 mb-2">
+                  <div className="col-sm-4 mt-2 mb-2">
                     <div className="card">
                       <div className="card-body">
                         <h5 className="card-title">{nome} - {estado}</h5>
